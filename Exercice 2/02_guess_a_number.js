629f69f42438268863fc70681710f4be76cb3b61
boris-valero <157314735+boris-valero@users.noreply.github.com>
2024-02-20 21:27:55 +0100

    Delete Exercice 2/02_guess_a_number.js

--- Exercice 2/02_guess_a_number.js
+++
@@ -1,9 +0,0 @@
-let guess_a_number;
-
-guess_a_number = Number(window.prompt('Choisis un nombre'));
-while (guess_a_number != 22) {
-  window.alert('Ceci n\'est pas la bonne réponse');
-  guess_a_number = Number(window.prompt('Quel est votre âge ?'));
-}
-if (guess_a_number == 22) {
-  window.alert('Ceci est la bonne réponse');
