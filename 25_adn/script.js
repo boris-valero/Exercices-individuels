153dd2a89bfde83095a5977e95661c2947fd53ec
linuxlite <val_b@orange.fr>
2024-08-24 21:29:54 +0200

    partie_b_étape_1

--- 25_adn/script.js
+++ 25_adn/script.js
@@ -22,22 +22,70 @@ console.log(transform_in_codons(str));
 //Étape 2
 
 const codon_to_proteine = {
-  ATA: "I",   ATC: "I",  ATT: "I",   ATG: "M", 
-  ACA: "T",   ACC: "T",  ACG: "T",   ACT: "T",
-  AAC: "N",   AAT: "N",  AAA: "K",   AAG: "K",
-  AGC: "S",   AGT: "S",  AGA: "R",   AGG: "R",
-  CTA: "L",   CTC: "L",  CTG: "L",   CTT: "L",
-  CCA: "P",   CCC: "P",  CCG: "P",   CCT: "P",
-  CAC: "H",   CAT: "H",  CAA: "Q",   CAG: "Q",
-  CGA: "R",   CGC: "R",  CGG: "R",   CGT: "R",
-  GTA: "V",   GTC: "V",  GTG: "V",   GTT: "V",
-  GCA: "A",   GCC: "A",  GCG: "A",   GCT: "A",
-  GAC: "D",   GAT: "D",  GAA: "E",   GAG: "E",
-  GGA: "G",   GGC: "G",  GGG: "G",   GGT: "G",
-  TCA: "S",   TCC: "S",  TCG: "S",   TCT: "S",
-  TTC: "F",   TTT: "F",  TTA: "L",   TTG: "L",
-  TAC: "Y",   TAT: "Y",  TAA: "_",   TAG: "_",
-  TGC: "C",   TGT: "C",  TGA: "_",   TGG: "W",
+  ATA: "I",
+  ATC: "I",
+  ATT: "I",
+  ATG: "M",
+  ACA: "T",
+  ACC: "T",
+  ACG: "T",
+  ACT: "T",
+  AAC: "N",
+  AAT: "N",
+  AAA: "K",
+  AAG: "K",
+  AGC: "S",
+  AGT: "S",
+  AGA: "R",
+  AGG: "R",
+  CTA: "L",
+  CTC: "L",
+  CTG: "L",
+  CTT: "L",
+  CCA: "P",
+  CCC: "P",
+  CCG: "P",
+  CCT: "P",
+  CAC: "H",
+  CAT: "H",
+  CAA: "Q",
+  CAG: "Q",
+  CGA: "R",
+  CGC: "R",
+  CGG: "R",
+  CGT: "R",
+  GTA: "V",
+  GTC: "V",
+  GTG: "V",
+  GTT: "V",
+  GCA: "A",
+  GCC: "A",
+  GCG: "A",
+  GCT: "A",
+  GAC: "D",
+  GAT: "D",
+  GAA: "E",
+  GAG: "E",
+  GGA: "G",
+  GGC: "G",
+  GGG: "G",
+  GGT: "G",
+  TCA: "S",
+  TCC: "S",
+  TCG: "S",
+  TCT: "S",
+  TTC: "F",
+  TTT: "F",
+  TTA: "L",
+  TTG: "L",
+  TAC: "Y",
+  TAT: "Y",
+  TAA: "_",
+  TAG: "_",
+  TGC: "C",
+  TGT: "C",
+  TGA: "_",
+  TGG: "W",
 };
 
 function translate_codon_to_protein(codons) {
@@ -49,5 +97,25 @@ function translate_codon_to_protein(codons) {
   return proteins;
 }
 
-let codons = ['TAA', 'TAG', 'TGC', 'TGG', 'TCA'];
+let codons = ["TAA", "TAG", "TGC", "TGG", "TCA"];
 console.log(translate_codon_to_protein(codons));
+
+//PARTIE B
+
+//Étape 1
+
+function make_group_of_25(str) {
+  let groupes = [];
+  for (let i = 0; i < str.length; i += 25) {
+    let groupe = [];
+    for (let j = 0; j < 25 && i + j < str.length; j++) {
+      groupe += str[i + j];
+    }
+    groupes.push(groupe);
+  }
+  for (let i = 0; i < groupes.length; i++) {
+    console.log(groupes[i]);
+  }
+}
+
+console.log(make_group_of_25(str));
